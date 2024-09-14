d03201bc2607609f2bdce55de49c79d6b02490fd
weu-souza <weuller1561@gmail.com>
2024-09-14 19:11:20 -0300

    fix: fixing type errors and update button choose image

--- ihc-prototype/src/components/RegisterProduct/index.tsx
+++ ihc-prototype/src/components/RegisterProduct/index.tsx
@@ -9,10 +9,10 @@ export function ProductForm() {
     formState: { errors },
   } = useForm<IProductsForm>();
 
-  const [preview, setPreview] = useState(null);
+  const [preview, setPreview] = useState<string>("");
 
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
     if (file) {
       setPreview(URL.createObjectURL(file));
     }
@@ -25,9 +25,9 @@ export function ProductForm() {
 
   return (
     <main>
-      <form className="flex flex-col gap-8 w-[350px] md:w-[400px]  lg:w-[650px] shadow-2xl shadow-cor-shadow py-10 px-5 md:px-20 rounded-xl">
-        <div className="lg:flex lg:justify-between">
-          <label className="label flex flex-col gap-2 mb-5 lg:mb-0">
+      <form className="flex flex-col gap-8 w-[350px] md:w-[400px]  lg:w-[1200px] shadow-2xl shadow-cor-shadow py-10 px-5 md:px-20 rounded-xl">
+        <div className="lg:flex gap-5">
+          <label className="label flex flex-col gap-2 mb-5 lg:mb-0 flex-auto">
             Título do Produto
             <input
               className="input"
@@ -37,7 +37,7 @@ export function ProductForm() {
               placeholder="Título do Produto"
             />
           </label>
-          <label className="label flex flex-col gap-2">
+          <label className="label flex flex-col gap-2 flex-auto">
             Descrição
             <input
               className="input"
@@ -73,30 +73,30 @@ export function ProductForm() {
               })}
               onChange={handleImageChange}
             />
-
             {/* Botão customizado */}
-            <div className="flex items-center">
+            <div className="flex items-center mx-auto">
+              
+            </div>
+          </label>
+
+          {preview?(
+            <div>
+              <img src={preview} alt="Preview" className="lg:w-[300px] lg:h-[300px] w-[200px] h-[200px] mx-auto rounded-sm" />
+            </div>
+          ):(
               <button
                 type="button"
-                className="w-full py-3 px-20 bg-blue-900 rounded-lg font-Poppins font-normal text-base text-white  cursor-pointer"
+                className="border-2 border-[currentColor] border-dashed lg:w-[400px] lg:h-[300px] w-[300px] h-[200px]  mx-auto font-Poppins font-normal text-base bg-[#ddd] text-[#aaa]"
                 onClick={handleButtonClick}
               >
                 Escolher arquivo
               </button>
-              <span>{ }</span>
-            </div>
-          </label>
-
-          {preview && (
-            <div>
-              <img src={preview} alt="Preview" className="w-50 h-auto" />
-            </div>
           )}
         </div>
         <input
           type="submit"
           value={"Cadastrar"}
-          className="py-3 px-20 bg-black rounded-lg font-Poppins font-normal text-base text-white  cursor-pointer"
+          className="py-3 px-20 bg-black rounded-lg font-Poppins font-normal text-base text-white  cursor-pointer lg:self-end"
         />
       </form>
     </main>
